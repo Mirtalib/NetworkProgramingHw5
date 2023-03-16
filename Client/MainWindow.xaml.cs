0d0cda5b412890ccc0deb8b273401abe74cee66c
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2023-03-16 22:20:51 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient client;
+        FileStream fileStream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var ipString = IPAddress.Parse("127.0.0.1");
+            var port = 27001;
+            IPEndPoint ep = new(ipString, port);
+            // fileStream = new FileStream("data.json", FileMode.Create, FileAccess.Write);
+            client = new TcpClient(ep);
+
+            cmboxCommandName.Items.Add(HttpMethods.GET.ToString());
+            cmboxCommandName.Items.Add(HttpMethods.POST.ToString());
+            cmboxCommandName.Items.Add(HttpMethods.PUT.ToString());
+            cmboxCommandName.Items.Add(HttpMethods.DELETE.ToString());
+        }
+        private void cmboxCommandName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmboxCommandName.SelectedIndex == 0)
+            {
+                txtboxMake.Visibility = Visibility.Collapsed;
+                txtboxModel.Visibility = Visibility.Collapsed;
+                txtboxVinCod.Visibility = Visibility.Collapsed;
+                txtboxYear.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Car car = new();
+            var serverStream = client.GetStream();
+            fileStream = new FileStream("data.json", FileMode.Open, FileAccess.Write);
+
+            
+
+            
+
+            
+            //fileStream.Flush();
+            //serverStream.CopyTo(fileStream);
+
+
+        }
+
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using Server.Models;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+
+List<Car> cars = new List<Car>(); 
+
+var ip = IPAddress.Parse("127.0.0.1");
+
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+
+listener.Start(10);
+
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var clientStream = client.GetStream();
+
+
+
+
+}
+
+
+
+Car? GetById(int id)
+{
+    foreach (var car in cars)
+    {
+        if (car.Id == id)
+        {
+            return car;
+        }
+    }
+    return null;
+}
+
+List<Car>? GetAll()
+{
+    return cars;
+}
+bool Add(Car car)
+{
+    if (car.VIN != null 
+        && car.Make != null
+        && car.Model != null
+        && car.Year != null
+        && car.Color != null)
+    {
+        cars.Add(car);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+
+}
+bool Update(Car car)
+{
+    return true;
+}
+bool Delete(int id)
+{
+    foreach (var car in cars)
+    {
+        if (car.Id == id)
+        {
+            cars.Remove(car);
+            return true;
+        }
+    }
+    return false;
+}
